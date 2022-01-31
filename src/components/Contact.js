9692fb2ff6f7036aff7102077dcfcf99a6f492cc
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-01-31 02:39:23 -0300

    Form bug fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
@@ -10,7 +10,6 @@ import Send from "@material-ui/icons/Send";
 import Particles from "react-particles-js";
 import { Fade } from "react-reveal";
 
-import { Snackbar } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   contactContainer: {
@@ -29,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
     transform: "translate(-50%, -50%)",
     position: "absolute",
     zIndex: "999",
-    width: "50vw"
+    width: "50vw",
   },
   input: {
     color: "#fff",
@@ -45,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
   particlesCanvas: {
     position: "fixed",
     height: "100vh",
-    opacity: "0.9"
+    opacity: "0.9",
   },
 }));
 
@@ -74,136 +73,120 @@ const InputField = withStyles({
 
 const Contact = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
   return (
     <Box component="div" className={`${classes.typedContainer} fondoContact`}>
       <Grid container justify="center">
-        
-        <Box component="form" name="contact-form" method="POST" data-netlify="true" className={classes.form}>
+        <Box
+          component="form"
+          name="contact-form"
+          method="POST"
+          data-netlify="true"
+          className={classes.form}
+        >
           <Fade duration={1000}>
-          <Typography variant="h5" className={classes.heading}>
-            Hire or Contact me...
-          </Typography>
-          <input type="hidden" name="form-name" value="contact-form" />
-          <InputField
-            fullWidth={true}
-            type="text"
-            label="Name"
-            name="name"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-            required
-          />
-          <InputField
-            fullWidth={true}
-            type="email"
-            label="Email"
-            name="email"
-            variant="outlined"
-            inputProps={{ className: classes.input }}
-            className={classes.field}
-            required
-          />
-          <InputField
-            fullWidth={true}
-            label="Message"
-            name="message"
-            variant="outlined"
-            multiline
-            rows={4}
-            inputProps={{ className: classes.input }}
-            required
-          />
-          <div data-netlify-recaptcha="true"></div>
-          <Button
-            onSubmit={handleClick}
-            type="submit"
-            variant="outlined"
-            fullWidth={true}
-            endIcon={<Send />}
-            className={classes.button}
-          >
-            Contact Me
-          </Button>
+            <Typography variant="h5" className={classes.heading}>
+              Hire or Contact me...
+            </Typography>
+            <input type="hidden" name="form-name" value="contact-form" />
+            <InputField
+              fullWidth={true}
+              type="text"
+              label="Name"
+              name="name"
+              variant="outlined"
+              inputProps={{ className: classes.input }}
+              required
+            />
+            <InputField
+              fullWidth={true}
+              type="email"
+              label="Email"
+              name="email"
+              variant="outlined"
+              inputProps={{ className: classes.input }}
+              className={classes.field}
+              required
+            />
+            <InputField
+              fullWidth={true}
+              label="Message"
+              name="message"
+              variant="outlined"
+              multiline
+              rows={4}
+              inputProps={{ className: classes.input }}
+              required
+            />
+            <div data-netlify-recaptcha="true"></div>
+            <Button
+              type="submit"
+              variant="outlined"
+              fullWidth={true}
+              endIcon={<Send />}
+              className={classes.button}
+            >
+              Contact Me
+            </Button>
           </Fade>
         </Box>
-        <Button variant="outlined"  onClick={handleClick}>
-          modalll
-        </Button>
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-              The message was sent!
-            </alert>
-          </Snackbar>
       </Grid>
 
       <Particles
         canvasClassName={classes.particlesCanvas}
         params={{
-          "particles": {
-              "number": {
-                  "value": 120,
-                  "density": {
-                      "enable": false
-                  }
+          particles: {
+            number: {
+              value: 180,
+              density: {
+                enable: false,
               },
-              "size": {
-                  "value": 3,
-                  "random": true,
-                  "anim": {
-                      "speed": 4,
-                      "size_min": 0.3
-                  }
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
               },
-              "line_linked": {
-                  "enable": false
-              },
-              "move": {
-                  "random": true,
-                  "speed": 1,
-                  "direction": "bottom",
-                  "out_mode": "out"
-              }
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+              direction: "bottom",
+              out_mode: "out",
+            },
           },
-          "interactivity": {
-              "events": {
-                  "onhover": {
-                      "enable": true,
-                      "mode": "bubble"
-                  },
-                  "onclick": {
-                      "enable": true,
-                      "mode": "repulse"
-                  }
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "bubble",
               },
-              "modes": {
-                  "bubble": {
-                      "distance": 250,
-                      "duration": 2,
-                      "size": 0,
-                      "opacity": 0
-                  },
-                  "repulse": {
-                      "distance": 400,
-                      "duration": 4
-                  }
-              }
-          }
-      }} />
+              onclick: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
     </Box>
-
   );
 };
 
